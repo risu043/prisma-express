f89360d9f163b2235bb7d681ef7700f089e29aaf
mai <uedamai813@gmail.com>
2025-01-12 21:46:29 +0900

    add hitCount for getUsers

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,22 +1,65 @@
 import { databaseManager } from '../db';
 const prisma = databaseManager.getInstance();
 
-export const getUsers = async () => {
-  const data = await prisma.user.findMany({
+export const getUsers = async (page: number) => {
+  const users = await prisma.user.findMany({
     orderBy: {
       score: 'desc',
     },
+    take: 20,
+    skip: (page - 1) * 20,
   });
 
-  return data;
+  let currentRank = 1;
+  let previousScore: number | null = null;
+
+  const usersWithRank = users.map((user, index) => {
+    if (user.score !== previousScore) {
+      currentRank = index + 1;
+    }
+
+    previousScore = user.score;
+
+    return {
+      ...user,
+      rank: currentRank,
+    };
+  });
+
+  const hitCount = await prisma.user.count();
+
+  return { usersWithRank, hitCount };
 };
 
 export const createUser = async (name: string, score: number) => {
-  const data = await prisma.user.create({
-    data: {
-      name: name,
-      score: score,
-    },
+  // トランザクションを使用して、ユーザー作成とランキング取得を整合性を保って実行
+  const result = await prisma.$transaction(async (tx) => {
+    // ユーザーを作成
+    const newUser = await tx.user.create({
+      data: {
+        name: name,
+        score: score,
+      },
+    });
+
+    // 新しいユーザーのランキングを取得
+    // gtは「greater than」で、作成したscoreより高得点のuserをcountする
+    const userRanking = await tx.user.count({
+      where: {
+        score: {
+          gt: score,
+        },
+      },
+    });
+
+    // 高得点のuserの人数 + 1 = 作成したuserのランク
+    const rank = userRanking + 1;
+
+    return {
+      ...newUser,
+      rank: rank,
+    };
   });
-  return data;
+
+  return result;
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { createUser, getUsers } from '../models/user';
 import { body, validationResult } from 'express-validator';
+import { DEFAULT_PAGE } from '../constants';
 
 export const usersRouter = express.Router();
 
 usersRouter.get('/', async (req: any, res: any) => {
-  const data = await getUsers();
+  const page = req.query['page'] ? Number(req.query['page']) : DEFAULT_PAGE;
+  const data = await getUsers(page);
 
   res.json(data);
 });
